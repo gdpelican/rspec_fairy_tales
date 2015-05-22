4cea0bcd9b08944a0484d5226897ea235c0ff495
James Kiesel <james.kiesel@gmail.com>
2015-05-22 17:24:49 +0700

    Whoops, look in right place for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 def find_stories(story_name)
   [].tap do |result|
-    Dir.glob("prompts/#{story_name}/*.txt") do |story|
+    Dir.glob("library/#{story_name}/*.txt") do |story|
       result << File.read(story)
     end
   end
